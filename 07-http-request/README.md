7101b35aef4b433c7343f1cea519d3d471212499
techarm <techarmdev@gmail.com>
2022-09-25 21:01:08 +0900

    Mock an API server with Mockoon and make http requests to add movies

---
+++ 07-http-request/README.md
@@ -0,0 +1,5 @@
+# Use mockoon cli to create mock server.
+
+```
+npm run mockoon start --data ./mock/movies.json
+```
--- 07-http-request/src/App.js
+++ 07-http-request/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -8,22 +9,24 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      // const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("http://localhost:3001/api/films");
       if (!response.ok) {
         throw new Error("Can not get movies data from server.");
       }
 
       const data = await response.json();
+      console.log(data);
       const transformedMovies = data.results.map((moviesData) => {
         return {
-          id: moviesData.episode_id,
+          id: moviesData.id,
           title: moviesData.title,
-          openingText: moviesData.opening_crawl,
-          releaseDate: moviesData.release_date,
+          openingText: moviesData.openingText,
+          releaseDate: new Date(moviesData.releaseDate).toLocaleDateString(),
         };
       });
       setMovies(transformedMovies);
@@ -31,24 +34,44 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch("http://localhost:3001/api/films", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    alert("Data was saved!");
+    console.log(data);
   }
 
   let content = <p>Found no movies</p>;
 
-  if (error) {
-    content = <p>{error}</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
 
   if (isLoading) {
     content = <p>Loading...</p>;
   }
 
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
+  if (error) {
+    content = <p>{error}</p>;
   }
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
