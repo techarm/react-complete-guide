1d24b51ec4219c52c1c4b413eabf86625896b902
techarm <techarmdev@gmail.com>
2022-09-25 14:33:10 +0900

    Add Error Boyndary example

--- 06-class-based-component/src/components/UserFinder.js
+++ 06-class-based-component/src/components/UserFinder.js
@@ -2,6 +2,7 @@ import { Fragment, Component } from "react";
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoyndary from "./ErrorBoundary";
 
 class UserFinder extends Component {
   static contextType = UsersContext;
@@ -39,7 +40,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoyndary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoyndary>
       </Fragment>
     );
   }
