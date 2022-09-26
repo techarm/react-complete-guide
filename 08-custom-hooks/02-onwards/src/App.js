0c876e086366619a05d5d7c72ab9926c0bbc2541
techarm <techarmdev@gmail.com>
2022-09-26 22:44:21 +0900

    Add custom http hooks

--- 08-custom-hooks/02-onwards/src/App.js
+++ 08-custom-hooks/02-onwards/src/App.js
@@ -1,43 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-ea6d1-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
-      );
+  const transformTasks = useCallback((data) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
+    for (const taskKey in data) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
     }
-    setIsLoading(false);
-  };
+
+    setTasks(loadedTasks);
+  }, []);
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    useMemo(() => {
+      return {
+        url: "https://react-http-ea6d1-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      };
+    }, []),
+    transformTasks
+  );
 
   useEffect(() => {
+    console.log("1");
     fetchTasks();
-  }, []);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
