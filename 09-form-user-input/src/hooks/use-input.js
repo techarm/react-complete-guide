c62b0d62b2ec0974aaac498ef530b459220bd468
techarm <techarmdev@gmail.com>
2022-09-27 23:48:19 +0900

    Rewrite input-hooks with useReducer()

--- 09-form-user-input/src/hooks/use-input.js
+++ 09-form-user-input/src/hooks/use-input.js
@@ -1,27 +1,58 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return {
+      ...state,
+      value: action.value,
+    };
+  }
+
+  if (action.type === "BLUR") {
+    return {
+      ...state,
+      isTouched: true,
+    };
+  }
+
+  if (action.type === "RESET") {
+    return {
+      value: "",
+      isTouched: false,
+    };
+  }
+
+  return initialInputState;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const valueIsValid = validateValue(enteredValue.trim());
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(inputState.value.trim());
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
   };
 
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
   };
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError: hasError,
     valueChangeHandler: valueChangeHandler,
