d31f92e932598ef73fe899e68c96bf5c925ef695
techarm <techarmdev@gmail.com>
2022-09-28 23:53:30 +0900

    Get data from the server.

--- 10-food-order-http-forms/src/components/Meals/AvailableMeals.js
+++ 10-food-order-http-forms/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,44 @@
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-ea6d1-default-rtdb.asia-southeast1.firebasedatabase1.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Unable to get data from server.");
+      }
+
+      const jsonData = await response.json();
+      let mealsDatas = [];
+      for (let key in jsonData) {
+        mealsDatas.push({
+          id: key,
+          name: jsonData[key].name,
+          description: jsonData[key].description,
+          price: jsonData[key].price,
+        });
+      }
+      setMeals(mealsDatas);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -43,7 +51,9 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && <ul>{mealsList}</ul>}
+        {error && <p>{error}</p>}
       </Card>
     </section>
   );
