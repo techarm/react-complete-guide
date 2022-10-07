5a5179c401c6af18e5d927ce694c3383b3308fea
techarm <techarmdev@gmail.com>
2022-10-07 22:13:49 +0900

    Using useEffect with Redux

--- 12-redux-advanced/src/components/Shop/ProductItem.js
+++ 12-redux-advanced/src/components/Shop/ProductItem.js
@@ -1,14 +1,51 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/cart-slice";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { id, title, price, description } = props;
 
   const addToCartHandler = () => {
+    // const newTotalQuantity = cart.totalQuantity + 1;
+
+    // const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+    // const existingItem = updatedItems.find((item) => item.id === id);
+    // if (existingItem) {
+    //   // ↓ This is is got a error. Uncaught TypeError: Cannot assign to read only property 'quantity' of object '#<Object>'
+    //   // existingItem.quantity++;
+    //   // existingItem.totalPrice = existingItem.totalPrice + price;
+
+    //   const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+    //   updatedItem.quantity++;
+    //   updatedItem.totalPrice = updatedItem.totalPrice + price;
+    //   const existingItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === id
+    //   );
+    //   updatedItems[existingItemIndex] = updatedItem;
+    // } else {
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //   });
+    // }
+
+    // const newCart = {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems,
+    // };
+
+    // dispatch(cartActions.replaceCart(newCart));
+
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
     dispatch(
       cartActions.addItemToCart({
         id,
