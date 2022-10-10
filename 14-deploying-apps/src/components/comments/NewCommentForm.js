ee45f2cc821c4f99479b5f1254f4c92a3b09e8cc
techarm <techarmdev@gmail.com>
2022-10-10 21:02:10 +0900

    Add starting project

---
+++ 14-deploying-apps/src/components/comments/NewCommentForm.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./NewCommentForm.module.css";
+
+const NewCommentForm = (props) => {
+  const [commentText, setCommentText] = useState("");
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
+
+  const commentChangeHandler = (event) => {
+    setCommentText(event.target.value);
+  };
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    // optional: Could validate here
+
+    // send comment to server
+    sendRequest({
+      quoteId: props.quoteId,
+      commentData: { text: commentText },
+    });
+
+    // clear up comment text
+    setCommentText("");
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
+      <div className={classes.control} onSubmit={submitFormHandler}>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea
+          id="comment"
+          rows="5"
+          value={commentText}
+          onChange={commentChangeHandler}
+        ></textarea>
+      </div>
+      <div className={classes.actions}>
+        <button className="btn">Add Comment</button>
+      </div>
+    </form>
+  );
+};
+
+export default NewCommentForm;
