0bec96be94d241a45b9a38614ee54a660930ffe3
techarm <techarmdev@gmail.com>
2022-10-11 22:30:00 +0900

    Add login and logout function

--- 15-authentication/src/components/Auth/AuthForm.js
+++ 15-authentication/src/components/Auth/AuthForm.js
@@ -1,28 +1,87 @@
-import { useState } from "react";
+import { useState, useRef, useContext } from "react";
 
+import AuthContext from "../../store/auth-context";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const authContext = useContext(AuthContext);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // Add validation
+
+    setIsLoading(true);
+    let url = isLogin
+      ? "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCqjetT15_wQlB_GVijyVNL_vTvp3RaDos"
+      : "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCqjetT15_wQlB_GVijyVNL_vTvp3RaDos";
+
+    fetch(url, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+    })
+      .then(async (res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          // show an error modal
+          const data = await res.json();
+          let errorMessage = "Authentication failed";
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage);
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        authContext.login(data.idToken);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" ref={emailInputRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            ref={passwordInputRef}
+            required
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
--- 15-authentication/src/components/Layout/MainNavigation.js
+++ 15-authentication/src/components/Layout/MainNavigation.js
@@ -1,24 +1,36 @@
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const authContext = useContext(AuthContext);
+  const { isLoggedIn } = authContext;
+
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <React.Fragment>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <button onClick={() => authContext.logout()}>Logout</button>
+              </li>
+            </React.Fragment>
+          )}
         </ul>
       </nav>
     </header>
